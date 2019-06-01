effc607185194e0a6ad26e835383f1c565f26ef3
filippilif155 <filippilif18@gmail.com>
2019-06-01 16:58:41 +0200

    Zadatak 2

--- zad2.py
+++ zad2.py
@@ -1,5 +1,6 @@
 city_temp_c = [("Podgorica", 20), ("Niksic", 15), ("Budva", 18), ("Cetinje", 16), ("Bijelo Polje", 11), ("Pljevlja", 12)]
 city_temp_f = map(lambda elem: (elem[0], (9 / 5) * elem[1] + 32), city_temp_c)
+'''
 print(list(city_temp_f))
             #MAP
 def map_prim(funct, podatak):
@@ -36,4 +37,13 @@ l = list(range(10))
 print(filter_prim(lambda num: num % 2 == 0,l))
 l = "asdASdasdD"
 print(filter_prim(lambda x: x.islower(), l))
-asdasfasfaaga
+'''
+def reduce_prim(funct, podatak):
+    rez = funct(podatak[0], podatak[1])
+    i = 2
+    while i != len(podatak):
+        rez = funct(rez,podatak[i])
+        i += 1
+    return rez
+print(reduce_prim(lambda x, y: x+y,[1,2,3,4]))
+print(reduce_prim(lambda a,b : a if a > b else b,[1,2,3,4]))
